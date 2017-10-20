6a2978ddc3d39f8e7f28cf228b97ce5f92376000
Dan Mack <mack@macktronics.com>
2017-10-19 22:04:42 -0500

    initial commit

---
+++ init.el
@@ -0,0 +1,42 @@
+;; mack basic emacs.el file; this just loads my full configuration
+;; from the .emacs.d subdirectory written in org mode
+
+(defconst emacs-start-time (current-time))
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+(setq package-enable-at-startup nil)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(setq package-enable-at-startup nil)
+(let ((elapsed (float-time
+                (time-subtract (current-time) emacs-start-time))))
+  (message "Loaded packages in %.3fs" elapsed))
+
+(require 'org)
+(org-babel-load-file
+ (expand-file-name "config-std.org" user-emacs-directory))
+
+;; Message how long it took to load everything (minus packages)
+(let ((elapsed (float-time (time-subtract (current-time)
+					  emacs-start-time))))
+  (message "Loading settings...done (%.3fs)" elapsed))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (powerline use-package rust-mode org markdown-mode magit))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
